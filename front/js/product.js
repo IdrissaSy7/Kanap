51e214df368fb340a1647c7d4ce5c3c542d2bc52
IdrissaSy7 <Kolo785@gmail.com>
2023-01-13 16:39:58 +0100

    Modification du panier

--- front/js/product.js
+++ front/js/product.js
@@ -10,37 +10,32 @@ console.log(productId);
 // Fonction qui permet d'ajouter un canapé avec ses attributs
 // images et alt, nom, prix, description et couleurs
 //
-async function main() {
-  await fetch("http://localhost:3000/api/products/" + productId)
-    .then((response) => response.json())
-    .then((article) => {
-      console.log(article);
+fetch("http://localhost:3000/api/products/" + productId)
+  .then((response) => response.json())
+  .then((article) => {
+    console.log(article);
 
-      document.title = article.name;
+    document.title = article.name;
 
-      const img = document.querySelector(".item__img");
-      img.innerHTML = `<img src="${article.imageUrl}" alt="${article.altTxt}">`;
+    const img = document.querySelector(".item__img");
+    img.innerHTML = `<img src="${article.imageUrl}" alt="${article.altTxt}">`;
 
-      const nomElement = document.querySelector("#title");
-      nomElement.innerText = article.name;
+    const nomElement = document.querySelector("#title");
+    nomElement.innerText = article.name;
 
-      const prixElement = document.querySelector("#price");
-      prixElement.innerText = article.price;
+    const prixElement = document.querySelector("#price");
+    prixElement.innerText = article.price;
 
-      const descriptionElement = document.querySelector("#description");
-      descriptionElement.innerText = article.description;
+    const descriptionElement = document.querySelector("#description");
+    descriptionElement.innerText = article.description;
 
-      const couleurElement = document.querySelector("#colors");
-      for (let i = 0; i < article.colors.length; i++) {
-        couleurElement.innerHTML =
-          couleurElement.innerHTML +
-          `<option value="${article.colors[i]}">${article.colors[i]}</option>`;
-      }
-    });
-}
-
-// Appel de la fonction.
-main();
+    const couleurElement = document.querySelector("#colors");
+    for (let i = 0; i < article.colors.length; i++) {
+      couleurElement.innerHTML =
+        couleurElement.innerHTML +
+        `<option value="${article.colors[i]}">${article.colors[i]}</option>`;
+    }
+  });
 
 // Obtention des données du produit
 fetch("http://localhost:3000/api/products/" + productId)
