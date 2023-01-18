c286d51cf5f65479b0382f71993391753ae484e9
IdrissaSy7 <Kolo785@gmail.com>
2023-01-18 15:14:38 +0100

    Modif panier

--- front/js/cart.js
+++ front/js/cart.js
@@ -8,7 +8,7 @@ for (let i = 0; i < panier.length; i++) {
     .then((response) => response.json())
     .then((produit) => {
       console.log(produit);
-      console.log(productId[i]);
+      console.log(productId);
 
       // Cette fonction ajoute les élements du panier à la section cart__items
       function createArticle() {
@@ -16,21 +16,20 @@ for (let i = 0; i < panier.length; i++) {
         let section = document.querySelector("#cart__items");
         let article = document.createElement("article");
         article.setAttribute("class", "cart__item");
-        article.setAttribute("data-id", productId[i]);
+        article.setAttribute("data-id", productId);
         article.setAttribute("data-color", panier[i].couleurElement);
         section.appendChild(article);
 
         // Crée la div image cart__item__img
-        let cart__items = document.querySelector(".cart__item");
         let cart__item__img = document.createElement("div");
         cart__item__img.setAttribute("class", "cart__item__img");
         cart__item__img.innerHTML = `<img src="${produit.imageUrl}" alt="${produit.altTxt}">`;
-        cart__items.appendChild(cart__item__img);
+        article.appendChild(cart__item__img);
 
         // Crée la div cart__item__content
         let cart__item__content = document.createElement("div");
         cart__item__content.setAttribute("class", "cart__item__content");
-        cart__items.appendChild(cart__item__content);
+        article.appendChild(cart__item__content);
 
         // Crée la div cart__item__content__description
         let cart__item__content__description = document.createElement("div");
@@ -38,10 +37,10 @@ for (let i = 0; i < panier.length; i++) {
           "class",
           "cart__item__content__description"
         );
-        cart__item__content__description.innerHTML = `                
-      <h2>${panier[i].nomElement}</h2>
-      <p>${panier[i].couleurElement}</p>
-      <p>${produit.price * panier[i].quantiteElement} €</p>`;
+        cart__item__content__description.innerHTML = `
+        <h2>${panier[i].nomElement}</h2>
+        <p>${panier[i].couleurElement}</p>
+        <p>${produit.price * panier[i].quantiteElement} €</p>`;
         cart__item__content.appendChild(cart__item__content__description);
 
         //   Crée la div cart__item__content__settings
@@ -60,9 +59,9 @@ for (let i = 0; i < panier.length; i++) {
           "cart__item__content__settings__quantity"
         );
         cart__item__content__settings__quantity.innerHTML = `
-      <p>Qté : </p>
-      <input type="number" class="itemQuantity" name="itemQuantity" min="1" max="100" value="${panier[i].quantiteElement}">
-      `;
+        <p>Qté : </p>
+        <input type="number" class="itemQuantity" name="itemQuantity" min="1" max="100" value="${panier[i].quantiteElement}">
+        `;
         cart__item__content__settings.appendChild(
           cart__item__content__settings__quantity
         );
@@ -75,8 +74,8 @@ for (let i = 0; i < panier.length; i++) {
           "cart__item__content__settings__delete"
         );
         cart__item__content__settings__delete.innerHTML = `
-      <p class="deleteItem">Supprimer</p>
-      `;
+        <p class="deleteItem">Supprimer</p>
+        `;
         cart__item__content__settings.appendChild(
           cart__item__content__settings__delete
         );
